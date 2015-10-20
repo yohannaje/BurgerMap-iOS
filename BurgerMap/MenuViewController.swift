285967c1c3cb43ff4ff43e76f375ad0c0a5c64ab
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-10-20 00:23:07 -0300

    First draft of sidebar menu

--- BurgerMap/MenuViewController.swift
+++ BurgerMap/MenuViewController.swift
@@ -71,8 +71,8 @@ class Configuration {
     
     static var UserImage: UIImage? {
         get {
-            guard let data = fetchKey(Key.UserImageKey.rawValue) as? NSData else { return nil }
-            return UIImage(data: data)
+        guard let data = fetchKey(Key.UserImageKey.rawValue) as? NSData else { return nil }
+        return UIImage(data: data)
         }
         
         set {
@@ -88,20 +88,88 @@ class Configuration {
     }
 }
 
-class MenuViewController: UIViewController {
-    
+class MenuHeaderView: UIView {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var reviewsLabel: UILabel!
+    @IBOutlet weak var checkinsLabel: UILabel!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.layer.cornerRadius = imageView.bounds.width / 2.0
+        imageView.layer.masksToBounds = true
+    }
+    
+    func updateView(name: String, image: UIImage?, reviews: Int, checkins: Int) {
+        dispatch_async(dispatch_get_main_queue()) {
+            [unowned self] in
+            self.imageView.image = image
+            self.nameLabel.text = {
+                _ -> String in
+                let components = name.componentsSeparatedByString(" ")
+                let lastName = components.last!
+                let forename = components.dropLast().joinWithSeparator(" ")
+                return "\(forename)\n\(lastName)"
+                }()
+            self.reviewsLabel.text = "\(reviews) reviews"
+            self.checkinsLabel.text = "\(checkins) check-ins"
+            self.layoutIfNeeded()
+            
+        }
+    }
+}
+
+struct MenuShortcut {
+    let action: String
+    let title: String
+    let iconName: String
+    lazy var icon: UIImage = {
+        //TODO: do this the right way here
+        return UIImage(named: self.iconName)!
+        }()
+    
+}
+
+class MenuViewController: UIViewController {
+    @IBOutlet weak var menuHeaderView: MenuHeaderView! {
+        didSet {
+            menuHeaderView.backgroundColor = .clearColor()
+        }
+    }
+    @IBOutlet weak var menuTableView: UITableView!
+    
+    let categories = ["Joints", "Veggie", "Delivery"]
+    let shortcuts = [
+        (action: "addReview", title: "Add Review"),
+        (action: "favorites", title: "Favorites"),
+        (action: "checkin", title: "Check In"),
+        (action: "settings", title: "Settings"),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        revealViewController().
+        menuTableView.tableHeaderView = menuHeaderView
+        menuTableView.contentInset = UIEdgeInsets(top: 66, left: 0, bottom: 0, right: 0)        
+        menuHeaderView.snp_makeConstraints { (make) -> Void in
+            make.width.equalTo(menuTableView.snp_width)
+        }
+        menuTableView.setNeedsLayout()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        fireProfileFetch()
+    }
+    
+    func fireProfileFetch() {
         let debug = false
-        
         let afterFetchProfile: PFUser.FetchProfileCallback = {
             [unowned self] name, image in
-            self.imageView.image = image
-            self.nameLabel.text = name
+            guard let name = name else {
+                NSLog("failed to get the user's name")
+                return
+            }
+            self.menuHeaderView.updateView(name, image: image, reviews: 0, checkins: 0)
         }
         
         afterFetchProfile(name: Configuration.UserName, picture: Configuration.UserImage)
@@ -122,31 +190,60 @@ class MenuViewController: UIViewController {
         }
     }
     
-    override func viewWillAppear(animated: Bool) {
-        view.snp_makeConstraints { (make) -> Void in
-            make.width.equalTo(revealViewController().rearViewRevealWidth)
-        }
-    }
-    
-    override func viewDidLayoutSubviews() {
-        imageView.layer.cornerRadius = imageView.bounds.width / 2.0
-        imageView.layer.masksToBounds = true
+    override func viewDidAppear(animated: Bool) {
+        NSLog("here")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+extension MenuViewController: UITableViewDataSource {
     
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+    enum MenuSections: Int {
+        case Filters = 0, Shortcuts
+        static var sectionCount: Int { return 2 }
     }
-    */
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return MenuSections.sectionCount
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = MenuSections(rawValue: section) else { return 0 }
+        switch section {
+        case .Filters:
+            return categories.count
+        case .Shortcuts:
+            return shortcuts.count
+        }
+    }
+    
+    private func filterCellForTableView(tableView: UITableView, atIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FilterCell", forIndexPath: indexPath)
+        let text = categories[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+        
+    }
+    
+    private func shortcutCellForTableView(tableView: UITableView, atIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ShortcutCell", forIndexPath: indexPath)
+        let (_, text) = shortcuts[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        guard let section = MenuSections(rawValue: indexPath.section) else { fatalError("requested a cell for a section that doesn't exist!") }
+        switch section {
+        case .Filters:
+            return filterCellForTableView(tableView, atIndexPath: indexPath)
+        case .Shortcuts:
+            return shortcutCellForTableView(tableView, atIndexPath: indexPath)
+        }
+    }
 }
